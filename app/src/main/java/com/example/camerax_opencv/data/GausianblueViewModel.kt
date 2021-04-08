7d60bcc1675dc5c2b526b78868d928441b9af770
momomomo111 <mossann0510@outlook.jp>
2021-04-08 14:26:19 +0900

    [clean] フラグメントの整理,  [add] titleタイトル画面の追加

--- app/src/main/java/com/example/camerax_opencv/data/GausianblueViewModel.kt
+++ app/src/main/java/com/example/camerax_opencv/data/GausianblueViewModel.kt
@@ -1,12 +1,8 @@
 package com.example.camerax_opencv.data
 
-import android.content.Context
-import androidx.annotation.StringRes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import com.example.camerax_opencv.R
 
 class GausianblurViewModel : ViewModel() {
     private val _kSize = MutableLiveData(1.0)
@@ -19,9 +15,11 @@ class GausianblurViewModel : ViewModel() {
     fun onKSizeChange(data: Double) {
         _kSize.value = data
     }
+
     fun onSigmaXChange(data: Double) {
         _sigmaX.value = data
     }
+
     fun onSigmaYChange(data: Double) {
         _sigmaY.value = data
     }
---
+++ app/src/main/java/com/example/camerax_opencv/ui/GaussianBlurFragment.kt
@@ -0,0 +1,264 @@
+package com.example.camerax_opencv.ui
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Surface
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.SeekBar
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModelProvider
+import com.example.camerax_opencv.data.GausianblurViewModel
+import com.example.camerax_opencv.databinding.FragmentGaussianblurBinding
+import com.google.common.util.concurrent.ListenableFuture
+import org.opencv.android.Utils
+import org.opencv.core.Core
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+import org.opencv.core.Size
+import org.opencv.imgproc.Imgproc
+import java.nio.ByteBuffer
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class GaussianBlurFragment : Fragment() {
+    private val viewModel by lazy { ViewModelProvider(this).get(GausianblurViewModel::class.java) }
+    private val REQUIRED_PERMISSIONS =
+        arrayOf("android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE")
+
+    /*** Views  */
+    private var previewView: PreviewView? = null
+    private var imageView: ImageView? = null
+
+    /*** For CameraX  */
+    private var camera: Camera? = null
+    private var preview: Preview? = null
+    private var imageAnalysis: ImageAnalysis? = null
+    private val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+
+
+    /*** Params */
+    private var kSize: Double = 1.0
+    private var sigmaX: Double = 0.0
+    private var sigmaY: Double = 0.0
+
+    companion object {
+
+        init {
+            System.loadLibrary("opencv_java4")
+        }
+    }
+
+    private var _binding: FragmentGaussianblurBinding? = null
+    private val binding get() = _binding!!
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentGaussianblurBinding.inflate(inflater, container, false)
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
+        previewView = binding.previewView
+        imageView = binding.imageView
+        val seekBarKSize = binding.seekBarKSize
+        val seekBarSigmaX = binding.seekBarSigmaX
+        val seekBarSigmaY = binding.seekBarSigmaY
+        seekBarKSize.max = 51
+        seekBarKSize.min = 1
+        seekBarSigmaX.max = 50
+        seekBarSigmaX.min = 0
+        seekBarSigmaY.max = 50
+        seekBarSigmaY.min = 0
+        if (checkPermissions()) {
+            startCamera()
+        } else {
+            ActivityResultContracts.RequestPermission()
+        }
+
+        fun onKSizeChange(data: Double) {
+            viewModel.onKSizeChange(data)
+        }
+
+        fun onSigmaXChange(data: Double) {
+            viewModel.onSigmaXChange(data)
+        }
+
+        fun onSigmaYChange(data: Double) {
+            viewModel.onSigmaYChange(data)
+        }
+        seekBarKSize.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                kSize = progress.toDouble()
+                if (progress.toDouble() % 2 == 0.0) {
+                    kSize += 1
+                }
+                onKSizeChange(kSize)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+        seekBarSigmaX.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                sigmaX = progress.toDouble()
+                onSigmaXChange(sigmaX)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+        seekBarSigmaY.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                sigmaY = progress.toDouble()
+                onSigmaYChange(sigmaY)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+        return binding.root
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
+            ProcessCameraProvider.getInstance(
+                requireContext()
+            )
+        val context: Context? = context
+        cameraProviderFuture.addListener(Runnable {
+            try {
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+                preview = Preview.Builder().build()
+                imageAnalysis = ImageAnalysis.Builder().build()
+                imageAnalysis!!.setAnalyzer(cameraExecutor, MyImageAnalyzer())
+                val cameraSelector =
+                    CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build()
+                cameraProvider.unbindAll()
+                camera = cameraProvider.bindToLifecycle(
+                    (context as LifecycleOwner),
+                    cameraSelector,
+                    preview,
+                    imageAnalysis
+                )
+                preview!!.setSurfaceProvider(previewView!!.createSurfaceProvider(camera!!.cameraInfo))
+            } catch (e: Exception) {
+                Log.e("error", "[startCamera] Use case binding failed", e)
+            }
+        }, ContextCompat.getMainExecutor(getContext()))
+    }
+
+    private inner class MyImageAnalyzer : ImageAnalysis.Analyzer {
+        override fun analyze(image: ImageProxy) {
+            /* Create cv::mat(RGB888) from image(NV21) */
+            val matOrg: Mat = getMatFromImage(image)
+
+            /* Fix image rotation (it looks image in PreviewView is automatically fixed by CameraX???) */
+            val mat: Mat = fixMatRotation(matOrg)
+
+            /* Do some image processing */
+            val matOutput = Mat(mat.rows(), mat.cols(), mat.type())
+            Imgproc.GaussianBlur(mat, matOutput, Size(kSize, kSize), sigmaX, sigmaY)
+
+
+            /* Convert cv::mat to bitmap for drawing */
+            val bitmap: Bitmap =
+                Bitmap.createBitmap(matOutput.cols(), matOutput.rows(), Bitmap.Config.ARGB_8888)
+            Utils.matToBitmap(matOutput, bitmap)
+
+            /* Display the result onto ImageView */
+            runOnUiThread { imageView?.setImageBitmap(bitmap) }
+
+            /* Close the image otherwise, this function is not called next time */
+            image.close()
+        }
+
+        private fun Fragment?.runOnUiThread(action: () -> Unit) {
+            this ?: return
+            if (!isAdded) return
+            activity?.runOnUiThread(action)
+        }
+
+        private fun getMatFromImage(image: ImageProxy): Mat {
+            /* https://stackoverflow.com/questions/30510928/convert-android-camera2-api-yuv-420-888-to-rgb */
+            val yBuffer: ByteBuffer = image.planes[0].buffer
+            val uBuffer: ByteBuffer = image.planes[1].buffer
+            val vBuffer: ByteBuffer = image.planes[2].buffer
+            val ySize: Int = yBuffer.remaining()
+            val uSize: Int = uBuffer.remaining()
+            val vSize: Int = vBuffer.remaining()
+            val nv21 = ByteArray(ySize + uSize + vSize)
+            yBuffer.get(nv21, 0, ySize)
+            vBuffer.get(nv21, ySize, vSize)
+            uBuffer.get(nv21, ySize + vSize, uSize)
+            val yuv = Mat(image.height + image.height / 2, image.width, CvType.CV_8UC1)
+            yuv.put(0, 0, nv21)
+            val mat = Mat()
+            Imgproc.cvtColor(yuv, mat, Imgproc.COLOR_YUV2RGB_NV21, 3)
+            return mat
+        }
+
+        private fun fixMatRotation(matOrg: Mat): Mat {
+            val mat: Mat
+            when (previewView!!.display.rotation) {
+                Surface.ROTATION_0 -> {
+                    mat = Mat(matOrg.cols(), matOrg.rows(), matOrg.type())
+                    Core.transpose(matOrg, mat)
+                    Core.flip(mat, mat, 1)
+                }
+                Surface.ROTATION_90 -> mat = matOrg
+                Surface.ROTATION_270 -> {
+                    mat = matOrg
+                    Core.flip(mat, mat, -1)
+                }
+                else -> {
+                    mat = Mat(matOrg.cols(), matOrg.rows(), matOrg.type())
+                    Core.transpose(matOrg, mat)
+                    Core.flip(mat, mat, 1)
+                }
+            }
+            return mat
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (context?.let {
+                    ContextCompat.checkSelfPermission(
+                        it,
+                        permission
+                    )
+                } != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+}
