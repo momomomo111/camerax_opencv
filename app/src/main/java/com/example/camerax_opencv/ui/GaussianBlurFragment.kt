5e3a7879a72cb3dc7dfa136a8cb834dbf60f1ebc
momomomo111 <mossann0510@outlook.jp>
2021-04-08 21:13:43 +0900

    [clean] Fragmentの一部を別ファイルに移行

--- app/src/main/java/com/example/camerax_opencv/ui/GaussianBlurFragment.kt
+++ app/src/main/java/com/example/camerax_opencv/ui/GaussianBlurFragment.kt
@@ -1,13 +1,11 @@
 package com.example.camerax_opencv.ui
 
 import android.content.Context
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.Surface
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -23,21 +21,19 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import com.example.camerax_opencv.data.GausianblurViewModel
 import com.example.camerax_opencv.databinding.FragmentGaussianblurBinding
+import com.example.camerax_opencv.util.checkPermissions
+import com.example.camerax_opencv.util.fixMatRotation
+import com.example.camerax_opencv.util.getMatFromImage
 import com.google.common.util.concurrent.ListenableFuture
 import org.opencv.android.Utils
-import org.opencv.core.Core
-import org.opencv.core.CvType
 import org.opencv.core.Mat
 import org.opencv.core.Size
 import org.opencv.imgproc.Imgproc
-import java.nio.ByteBuffer
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class GaussianBlurFragment : Fragment() {
     private val viewModel by lazy { ViewModelProvider(this).get(GausianblurViewModel::class.java) }
-    private val REQUIRED_PERMISSIONS =
-        arrayOf("android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE")
 
     /*** Views  */
     private var previewView: PreviewView? = null
@@ -69,7 +65,7 @@ class GaussianBlurFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentGaussianblurBinding.inflate(inflater, container, false)
         binding.viewmodel = viewModel
         binding.lifecycleOwner = this
@@ -84,7 +80,7 @@ class GaussianBlurFragment : Fragment() {
         seekBarSigmaX.min = 0
         seekBarSigmaY.max = 50
         seekBarSigmaY.min = 0
-        if (checkPermissions()) {
+        if (checkPermissions(context)) {
             startCamera()
         } else {
             ActivityResultContracts.RequestPermission()
@@ -152,7 +148,7 @@ class GaussianBlurFragment : Fragment() {
                 requireContext()
             )
         val context: Context? = context
-        cameraProviderFuture.addListener(Runnable {
+        cameraProviderFuture.addListener({
             try {
                 val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
                 preview = Preview.Builder().build()
@@ -181,7 +177,7 @@ class GaussianBlurFragment : Fragment() {
             val matOrg: Mat = getMatFromImage(image)
 
             /* Fix image rotation (it looks image in PreviewView is automatically fixed by CameraX???) */
-            val mat: Mat = fixMatRotation(matOrg)
+            val mat: Mat = fixMatRotation(matOrg, previewView)
 
             /* Do some image processing */
             val matOutput = Mat(mat.rows(), mat.cols(), mat.type())
@@ -206,59 +202,6 @@ class GaussianBlurFragment : Fragment() {
             activity?.runOnUiThread(action)
         }
 
-        private fun getMatFromImage(image: ImageProxy): Mat {
-            /* https://stackoverflow.com/questions/30510928/convert-android-camera2-api-yuv-420-888-to-rgb */
-            val yBuffer: ByteBuffer = image.planes[0].buffer
-            val uBuffer: ByteBuffer = image.planes[1].buffer
-            val vBuffer: ByteBuffer = image.planes[2].buffer
-            val ySize: Int = yBuffer.remaining()
-            val uSize: Int = uBuffer.remaining()
-            val vSize: Int = vBuffer.remaining()
-            val nv21 = ByteArray(ySize + uSize + vSize)
-            yBuffer.get(nv21, 0, ySize)
-            vBuffer.get(nv21, ySize, vSize)
-            uBuffer.get(nv21, ySize + vSize, uSize)
-            val yuv = Mat(image.height + image.height / 2, image.width, CvType.CV_8UC1)
-            yuv.put(0, 0, nv21)
-            val mat = Mat()
-            Imgproc.cvtColor(yuv, mat, Imgproc.COLOR_YUV2RGB_NV21, 3)
-            return mat
-        }
 
-        private fun fixMatRotation(matOrg: Mat): Mat {
-            val mat: Mat
-            when (previewView!!.display.rotation) {
-                Surface.ROTATION_0 -> {
-                    mat = Mat(matOrg.cols(), matOrg.rows(), matOrg.type())
-                    Core.transpose(matOrg, mat)
-                    Core.flip(mat, mat, 1)
-                }
-                Surface.ROTATION_90 -> mat = matOrg
-                Surface.ROTATION_270 -> {
-                    mat = matOrg
-                    Core.flip(mat, mat, -1)
-                }
-                else -> {
-                    mat = Mat(matOrg.cols(), matOrg.rows(), matOrg.type())
-                    Core.transpose(matOrg, mat)
-                    Core.flip(mat, mat, 1)
-                }
-            }
-            return mat
-        }
-    }
-
-    private fun checkPermissions(): Boolean {
-        for (permission in REQUIRED_PERMISSIONS) {
-            if (context?.let {
-                    ContextCompat.checkSelfPermission(
-                        it,
-                        permission
-                    )
-                } != PackageManager.PERMISSION_GRANTED) {
-                return false
-            }
-        }
-        return true
     }
 }
