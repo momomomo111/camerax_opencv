92c05123f7be38ac6073807429e4dc47c52f9221
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2022-02-14 23:50:17 +0900

    [delete] Delete LiveData that is not in use
    使用していないLiveDataを削除

--- app/src/main/java/com/example/camerax_opencv/data/GaussianblurViewModel.kt
+++ app/src/main/java/com/example/camerax_opencv/data/GaussianblurViewModel.kt
@@ -1,15 +1,12 @@
 package com.example.camerax_opencv.data
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 class GaussianblurViewModel : ViewModel() {
     private val _params = MutableStateFlow(Params.GaussianBlurParams(1.0, 0.0, 0.0))
     val params: StateFlow<Params.GaussianBlurParams> = _params
-    val liveParams: LiveData<Params.GaussianBlurParams> = _params.asLiveData()
 
     fun onKSizeChange(data: Double) {
         _params.value = _params.value.copy(kSize = data)
--- app/src/main/java/com/example/camerax_opencv/data/HsvextractionViewModel.kt
+++ app/src/main/java/com/example/camerax_opencv/data/HsvextractionViewModel.kt
@@ -1,8 +1,6 @@
 package com.example.camerax_opencv.data
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -10,7 +8,6 @@ class HsvextractionViewModel : ViewModel() {
     private val _params =
         MutableStateFlow(Params.HsvExtractionParams(255.0, 255.0, 255.0, 0.0, 0.0, 0.0))
     val params: StateFlow<Params.HsvExtractionParams> = _params
-    val liveParams: LiveData<Params.HsvExtractionParams> = _params.asLiveData()
 
     fun onUpperHChange(data: Double) {
         _params.value = _params.value.copy(upperH = data)
