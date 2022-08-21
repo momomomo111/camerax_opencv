3f5520f952ac748f4cad85a83379a1a6782afe51
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2022-08-21 23:45:44 +0900

    fix conflict

--- app/src/main/java/com/momomomo111/camerax_opencv/MainActivity.kt
+++ app/src/main/java/com/momomomo111/camerax_opencv/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.momomomo111.camerax_opencv
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.momomomo111.camerax_opencv.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +24,19 @@ class MainActivity : AppCompatActivity() {
             ).navController
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(binding.navView, navController)
+
+        val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
+
+        val vibSwitch = SwitchMaterial(this)
+        val vibrationEnable = sharedPref.getBoolean(getString(R.string.vibration_enable), true)
+        vibSwitch.isChecked = vibrationEnable
+        vibSwitch.setOnCheckedChangeListener { _, isChecked ->
+            with(sharedPref.edit()) {
+                putBoolean(getString(R.string.vibration_enable), isChecked)
+                apply()
+            }
+        }
+        binding.navView.menu.findItem(R.id.vibrationSwitch).actionView = vibSwitch
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -30,6 +45,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        init { System.loadLibrary("opencv_java4") }
+        init {
+            System.loadLibrary("opencv_java4")
+        }
     }
 }
