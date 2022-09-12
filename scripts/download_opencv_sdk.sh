7eca68e05bb34e6d1d7d193b85f1c90b2400ce96
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2022-09-13 03:38:31 +0900

    [fix]

--- scripts/download_opencv_sdk.sh
+++ scripts/download_opencv_sdk.sh
@@ -1,3 +1,3 @@
 /usr/local/opt/curl/bin/curl -L https://github.com/opencv/opencv/releases/download/4.6.0/opencv-4.6.0-android-sdk.zip -o /tmp/OpenCV-android-sdk.zip -s
-/usr/bin/unzip -q /tmp/OpenCV-android-sdk.zip -d ./OpenCV-android-sdk
+/usr/bin/unzip -q /tmp/OpenCV-android-sdk.zip
 /bin/rm /tmp/OpenCV-android-sdk.zip
