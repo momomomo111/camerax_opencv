61f174b19a2cf84da9d71206d8491b6614fc655d
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2024-07-14 15:01:52 +0900

    Fix workflow

--- .github/download_opencv_sdk.sh
+++ .github/download_opencv_sdk.sh
@@ -1,3 +1,3 @@
-/usr/local/opt/curl/bin/curl -L https://github.com/opencv/opencv/releases/download/4.6.0/opencv-4.6.0-android-sdk.zip -o /tmp/OpenCV-android-sdk.zip -s
+/usr/local/opt/curl/bin/curl -L https://github.com/opencv/opencv/releases/download/4.10.0/opencv-4.10.0-android-sdk.zip -o /tmp/OpenCV-android-sdk.zip -s
 /usr/bin/unzip -q /tmp/OpenCV-android-sdk.zip
 /bin/rm /tmp/OpenCV-android-sdk.zip
