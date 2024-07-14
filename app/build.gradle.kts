bdf8bb0398f0b20f1652291dddfbf63c5b186baa
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2024-07-14 14:36:12 +0900

    Upgrade gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    namespace = "com.momomomo111.camerax_opencv"
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
             jvmTarget = "1.8"
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,10 @@ buildscript {
         maven(url = "https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.android.tools.build:gradle:8.5.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.1")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.0")
     }
 }
 
