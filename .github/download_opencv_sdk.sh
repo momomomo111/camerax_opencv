e1d01ea9d8b71109151a64b4ede9fe1e8de1d5dc
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2024-07-14 15:05:57 +0900

    Fix workflow

--- .github/download_opencv_sdk.sh
+++ .github/download_opencv_sdk.sh
@@ -1,3 +1,10 @@
-/usr/local/opt/curl/bin/curl -L https://github.com/opencv/opencv/releases/download/4.10.0/opencv-4.10.0-android-sdk.zip -o /tmp/OpenCV-android-sdk.zip -s
+#!/bin/bash
+
+# curlコマンドのパスを修正
+/usr/bin/curl -L https://github.com/opencv/opencv/releases/download/4.10.0/opencv-4.10.0-android-sdk.zip -o /tmp/OpenCV-android-sdk.zip -s
+
+# unzipコマンドのパスを確認（通常は/usr/bin/unzipで問題ありません）
 /usr/bin/unzip -q /tmp/OpenCV-android-sdk.zip
+
+# rmコマンドのパスを確認（通常は/bin/rmで問題ありません）
 /bin/rm /tmp/OpenCV-android-sdk.zip
